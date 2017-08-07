4bec7566e53987317ce1e44767c554da6a411bb3
Jan-Thomas R <jt.riemenschneider@gmail.com>
2017-08-07 13:45:44 +0200

    Add files via upload
    Initial commit. Program works so far.

---
+++ ak47simulator.js
@@ -0,0 +1,65 @@
+window.onload = function () {
+    grabNewAK();
+}
+
+var bullets;
+var rounds;
+
+// Sounds
+var sound_shoot = new Audio("sounds/shoot.wav");
+var sound_reload_one = new Audio("sounds/reload_one.wav");
+var sound_reload_two = new Audio("sounds/reload_two.wav");
+var sound_grabNewAK = new Audio("sounds/grabNewAK.wav");
+
+function shoot() {
+	
+	if(!outOfBullets()) {
+		sound_shoot.pause();
+		sound_shoot.currentTime = 0;
+		sound_shoot.play();
+	}
+	
+	if(bullets !== 0) {
+		bullets--;
+		displayAmmo();
+	}
+}
+
+function reload() {
+	
+	if(!outOfRounds()) {
+		sound_reload_one.play();
+		sound_reload_two.play();
+	}
+
+	if(!isWeaponEmpty() && !outOfRounds()) {
+		bullets = 30;
+		rounds = rounds - 30;
+		displayAmmo();
+	}
+}
+
+function grabNewAK() {
+
+	sound_grabNewAK.play();
+	
+	bullets = 30;
+	rounds = 90;
+	displayAmmo();
+}
+
+function displayAmmo() {
+	document.getElementById("ammo").innerHTML = "( " + bullets + " | " + rounds + ")";
+}
+
+function isWeaponEmpty() {
+	return outOfBullets() && outOfRounds();
+}
+
+function outOfBullets() {
+	return bullets == 0;
+}
+
+function outOfRounds() {
+	return rounds == 0;
+}
